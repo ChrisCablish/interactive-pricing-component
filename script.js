84f1332500772c266241c750ccc5681bc2093ebc
Chris <chriscablish@hotmail.com>
2023-06-22 12:10:05 -0500

    complete javascript functionality, initialize SaSS

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// variables
 const slider = document.getElementById('customRange2');
 const pageViews = document.getElementById('pv__number');
 const dollars = document.getElementById('dollars');
+const toggle = document.getElementById('flexSwitchCheckDefault');
+let discount = false;
+
+// objects and functions 
 class Price {
     constructor (views, price) {
         this.views = views;
@@ -14,17 +19,50 @@ const priceStructure = [
     new Price(500000, 24),
     new Price(1000000, 36)
 ]
-const checkSlider = () => {
-    let priceSelection = Number(slider.value);
-    pageViews.innerText = priceStructure[priceSelection].views;
-    dollars.innerText = priceStructure[priceSelection].price;
+
+const updateSlider = () => {
+    let viewsNumber = priceStructure[slider.value].views;
+    let priceNumber = priceStructure[slider.value].price;
+
+    //price
+    if (discount) {
+        priceNumber = (priceNumber * 0.75);
+    }
+    dollars.innerText = priceNumber;
+
+    //views
+    if ([...String(viewsNumber)].length === 5) {
+        let viewsArray = [...String(viewsNumber)].slice(0,2);
+        viewsArray.push('K');
+        pageViews.innerText = viewsArray.join('');
+    } else if ([...String(viewsNumber)].length === 6) {
+        let viewsArray = [...String(viewsNumber)].slice(0,3);
+        viewsArray.push('K');
+        pageViews.innerText = viewsArray.join('');
+    } else {
+        let viewsArray = [...String(viewsNumber)].slice(0,1);
+        viewsArray.push('M');
+        pageViews.innerText = viewsArray.join('');
+    }  
 };
 
-checkSlider();
+// execution
+
+updateSlider();
+
+toggle.addEventListener('click', function() {
+    if (this.checked) {
+      discount = true;
+    } else if (!(this.checked)) {
+      discount = false;
+    }
+    updateSlider();
+  });
 
 slider.addEventListener('input', () => {
-    checkSlider();
+    updateSlider();
 });
+
 
 
 
